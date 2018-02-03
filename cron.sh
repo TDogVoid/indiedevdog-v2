674fe88fadcf4cd7e1d55224f49570dda315c8e7
Terrence Drumm <terrence.drumm@gmail.com>
2018-02-03 13:00:59 -0500

    cron file commands added

---
+++ cron.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+cd /Users/terrencedrumm/indiedevdog-v2/
+/usr/local/bin/node node_modules/.bin/electron ./scripts/cron.js
+
+#cron command
+#/Users/terrencedrumm/indiedevdog-v2/cron.sh >> /Users/terrencedrumm/indiedevdog-v2/cron.log
